45e91fc548974bfb3e020ab20937eb88a95ead34
KriseevM <mish.kryseev@gmail.com>
2022-08-21 01:03:07 +0300

    Add actual logic to RequestsController

--- gdrequests_api/Controllers/RequestsController.cs
+++ gdrequests_api/Controllers/RequestsController.cs
@@ -1,29 +1,44 @@
+using gdrequests_api.Data;
+using gdrequests_api.Data.Entities;
 using gdrequests_api.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace gdrequests_api.Controllers;
 
+[Route("[controller].[action]")]
 public class RequestsController : Controller
 {
-    private ILogger<RequestsController> _logger;
-    private GdLevelsChecker _levelsChecker;
-    public RequestsController(ILogger<RequestsController> logger, GdLevelsChecker levelsChecker)
+    private readonly ILogger<RequestsController> _logger;
+    private readonly GdLevelsChecker _levelsChecker;
+    private readonly MainDataContext _dbContext;
+
+    public RequestsController(ILogger<RequestsController> logger, GdLevelsChecker levelsChecker,
+        MainDataContext dbContext)
     {
         _logger = logger;
         _levelsChecker = levelsChecker;
+        _dbContext = dbContext;
     }
-    
-    [HttpPut("[controller].[action]")]
+
+    [HttpPut]
     public async Task<IActionResult> Add(int levelId)
     {
-        _logger.Log(LogLevel.Debug, "Received {1} level. Checking", levelId);
-        var levelExists = await _levelsChecker.CheckLevelExistenceAsync(levelId);
-        if (levelExists)
+        _logger.Log(LogLevel.Debug, "Received {0} level. Checking", levelId);
+        if (_dbContext.Levels.Any(p => p.ServerId == levelId))
         {
-            _logger.Log(LogLevel.Debug, "Level was found on GD server");
-            return StatusCode(201);
+            _logger.Log(LogLevel.Information, "Level {0} was rejected - duplicate", levelId);
+            return Conflict("Level already is in database");
         }
-        _logger.Log(LogLevel.Debug, "Level was not found on GD server");
-        return StatusCode(400);
+        var levelExists = await _levelsChecker.CheckLevelExistenceAsync(levelId);
+        if (!levelExists)
+        {
+            _logger.Log(LogLevel.Debug, "Level was not found on GD server");
+            return StatusCode(410, "There's no such level on Geometry Dash servers");
+        }
+        _logger.Log(LogLevel.Debug, "Level was found on GD server");
+        _dbContext.Levels.Add(new Level() { ServerId = levelId });
+        _logger.Log(LogLevel.Information, "Level {0} was added to database", levelId);
+        await _dbContext.SaveChangesAsync();
+        return Ok("ok");
     }
 }

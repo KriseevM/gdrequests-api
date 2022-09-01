1082a5fcc187d21da49ff6eef676efb2a83ed432
KriseevM <mish.kryseev@gmail.com>
2022-09-01 19:11:22 +0300

    Add GdServerController

---
+++ gdrequests_api/Controllers/GdServerController.cs
@@ -0,0 +1,28 @@
+using gdrequests_api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gdrequests_api.Controllers;
+
+[ApiController]
+[Route("gdserver.[action]")]
+public class GdServerController : ControllerBase
+{
+    private readonly GdLevelsChecker _checker;
+
+    public GdServerController(GdLevelsChecker checker)
+    {
+        _checker = checker;
+    }
+    
+    [HttpGet]
+    public IActionResult GetLevelInfo(int[] levelIds)
+    {
+        return StatusCode(501);
+    }
+    [HttpGet]
+    public async Task<bool> CheckLevel(int levelId)
+    {
+        bool result = await _checker.CheckLevelExistenceAsync(levelId);
+        return result;
+    }
+}
